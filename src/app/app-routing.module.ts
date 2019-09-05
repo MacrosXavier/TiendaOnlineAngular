f6025c4b9f7927f3dd6fe757e80c12bf792935c8
Mauro Sanchez <mjscontacto@gmail.com>
2019-09-05 12:28:20 -0300

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { TiendaComponent } from './tienda/tienda.component';
+import { DetalleProductoComponent } from './detalle-producto/detalle-producto.component';
+import { CarritoComponent } from './carrito/carrito.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'tienda', component: TiendaComponent },
+  { path: 'detalle-producto/:id', component: DetalleProductoComponent},
+  { path: 'carrito', component: CarritoComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'login' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireList, AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs';
+//import { log } from 'util';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'routing';
+
 }
